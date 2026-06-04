2113b2d935192d709655b41b92ef4e60b223f325
Maxim Tolpeev <tma2702@mail.ru>
2026-06-04 14:26:48 +0300

    fix: фзapp.module.ts

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -13,23 +13,22 @@ import { Suggestion } from './suggestion/suggestion.entity'
 
 @Module({
   imports: [
-    UserModule,
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
-        host: configService.get('DB_HOST'),
-        port: configService.get('DB_PORT'),
-        username: configService.get('DB_USERNAME'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_NAME'),
+        url: configService.get('DATABASE_URL'),
+        entities: [User, Blogger, Suggestion],
         synchronize: false,
         dropSchema: false,
-        entities: [User, Blogger, Suggestion],
+        ssl: configService.get('DATABASE_URL')?.includes('sslmode=require')
+          ? { rejectUnauthorized: false }
+          : false,
       }),
       inject: [ConfigService],
     }),
+    UserModule,
     AuthModule,
     BloggerModule,
     SuggestionModule,
