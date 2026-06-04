d1dafdbb17a0407de2f24891ddad4094586f56f1
Maxim Tolpeev <tma2702@mail.ru>
2026-06-04 14:49:06 +0300

    fixed app.module.ts

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
-import { AppService } './app.service'
+import { AppService } from './app.service'
 import { UserModule } from './user/user.module'
 import { ConfigModule, ConfigService } from '@nestjs/config'
 import { TypeOrmModule } from '@nestjs/typeorm'
