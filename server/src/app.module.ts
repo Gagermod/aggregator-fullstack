a930b8e776777f7323ce1a2525cf9dfa9ecc1cdd
Maxim Tolpeev <tma2702@mail.ru>
2026-06-04 14:45:21 +0300

    fixed app.module.ts

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
-import { AppService } from './app.service'
+import { AppService } './app.service'
 import { UserModule } from './user/user.module'
 import { ConfigModule, ConfigService } from '@nestjs/config'
 import { TypeOrmModule } from '@nestjs/typeorm'
@@ -10,30 +10,22 @@ import { User } from './user/entities/user.entity'
 import { Blogger } from './blogger/blogger.entity'
 import { SuggestionModule } from './suggestion/suggestion.module'
 import { Suggestion } from './suggestion/suggestion.entity'
-import * as fs from 'fs'
-import * as path from 'path'
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: (configService: ConfigService) => {
-        const caPath = path.join(process.cwd(), 'ca.pem')
-        const ca = fs.readFileSync(caPath).toString()
-
-        return {
-          type: 'postgres',
-          url: configService.get('DATABASE_URL'),
-          entities: [User, Blogger, Suggestion],
-          synchronize: false,
-          dropSchema: false,
-          ssl: {
-            rejectUnauthorized: true,
-            ca: ca,
-          },
-        }
-      },
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        url: configService.get('DATABASE_URL'),
+        entities: [User, Blogger, Suggestion],
+        synchronize: false,
+        dropSchema: false,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }),
       inject: [ConfigService],
     }),
     UserModule,
